eeb934c9ce67314ea5a06fb20d0a092257745350
mplouffe <matheuplouffe@gmail.com>
2020-08-15 11:48:06 -0500

    WIP. Adding state methods to state engine.

--- public/js/GameEngine/GameEngine_StateEngine.js
+++ public/js/GameEngine/GameEngine_StateEngine.js
@@ -19,14 +19,29 @@
  	}
  }
 
+ // push new state onto stack
+ // call cover state method to clean current statue UI
+ // and save any state that needs to be saved before stashing state
  StateStack.prototype.pushState = function(gameState){
+    if (!(this.currentState === undefined))
+    {
+        this.currentState.coverState();
+    }
  	this.stateStack.push(gameState);
  	this.currentState = this.stateStack[this.stateStack.length - 1];
  }
 
+ // pop current state off the stack
+ // call remove state method to clean current state UI
+ // and remove any other state garbage before it is trashed from the stack
  StateStack.prototype.popState = function(){
+    if (!(this.currentState === undefined))
+    {
+        this.currentState.removeState();
+    }
  	this.stateStack.pop();
- 	this.currentState = this.stateStack[this.stateStack.length - 1];
+     this.currentState = this.stateStack[this.stateStack.length - 1];
+    // refresh the new current state if needed
  	if(this.currentState != null && this.currentState.needsRefresh === true)
  	{
  		this.currentState.refresh();
